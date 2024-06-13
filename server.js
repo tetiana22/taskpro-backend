3561dd47b73c902952f2021e9ac8c992992afcbc
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-06-13 08:58:51 +0100

    yarn .lock server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const { DB_HOST, PORT } = process.env;
+
+if (!DB_HOST) {
+  console.error("DB_HOST is not defined. Check your .env file.");
+  process.exit(1);
+}
 
 mongoose
   .connect(DB_HOST)
