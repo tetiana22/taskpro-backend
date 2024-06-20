b9b798df2181963ad8494398fb65416fd5d9ab89
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-06-20 12:56:12 +0100

    change email

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -134,21 +134,28 @@ const getHelpEmail = async (req, res) => {
     subject: "User needs help",
     html: `<div><p>Registered user of the application: <strong>${name}</strong></p>
                 <p>Message from user: <strong>${comment}</strong></p>
-                <p>Send response to email: <strong>${email}</strong></p><div/>`,
+                <p>Send response to email: <strong>${email}</strong></p></div>`,
   };
-  await sendEmail(helpRequest);
-  const helpResponse = {
-    to: email,
-    subject: "Support",
-    html: `<div><p>Dear <strong>${name}</strong> !</p>
-                <p>We have received a message from you with the text: <strong>${comment}</strong>.</p>
-                <p>Thank you for you request! We will review your comment and respond as soon as possible.</p>`,
-  };
-  await sendEmail(helpResponse);
 
-  res.json({
-    message: "Reply email has been sent",
-  });
+  try {
+    await sendEmail(helpRequest);
+
+    const helpResponse = {
+      to: email,
+      subject: "Support",
+      html: `<div><p>Dear <strong>${name}</strong>!</p>
+                    <p>We have received a message from you with the text: <strong>${comment}</strong>.</p>
+                    <p>Thank you for your request! We will review your comment and respond as soon as possible.</p></div>`,
+    };
+
+    await sendEmail(helpResponse);
+
+    res.json({ message: "Reply email has been sent" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to send email", error: error.message });
+  }
 };
 
 export default {
--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,29 +2,29 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { GMAIL_EMAIL, GMAIL_PASSWORD } = process.env;
+const { YAHOO_EMAIL, YAHOO_PASSWORD } = process.env;
 
-if (!GMAIL_EMAIL || !GMAIL_PASSWORD) {
+if (!YAHOO_EMAIL || !YAHOO_PASSWORD) {
   console.error(
-    "GMAIL_EMAIL or GMAIL_PASSWORD is not defined in the environment variables."
+    "YAHOO_EMAIL or YAHOO_PASSWORD is not defined in the environment variables."
   );
   process.exit(1);
 }
 
 const nodemailerConfig = {
-  host: "smtp.gmail.com",
+  host: "smtp.mail.yahoo.com",
   port: 465,
   secure: true,
   auth: {
-    user: GMAIL_EMAIL,
-    pass: GMAIL_PASSWORD,
+    user: YAHOO_EMAIL,
+    pass: YAHOO_PASSWORD,
   },
 };
 
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: GMAIL_EMAIL };
+  const email = { ...data, from: YAHOO_EMAIL };
   try {
     await transport.sendMail(email);
   } catch (error) {
