196e871d7d9a13a3653acf33680ca7f47e505b7c
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-06-26 20:16:34 +0100

    change login token

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -45,7 +45,7 @@ const login = async (req, res) => {
 
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
-  await User.findByIdAndUpdate(_id, { token: "", avatarURL: null });
+  await User.findByIdAndUpdate(user._id, { token });
 
   res.status(200).json({
     token,
