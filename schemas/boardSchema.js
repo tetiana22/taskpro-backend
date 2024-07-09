519bdb67ff743e1df94ac6268b69fea0f05e2d7d
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-07-09 16:39:20 +0100

    add

--- schemas/boardSchema.js
+++ schemas/boardSchema.js
@@ -3,6 +3,7 @@ import Joi from "joi";
 export const createBoardSchema = Joi.object({
   title: Joi.string().required(),
   icon: Joi.string(),
+  background: Joi.string(),
 });
 
 export const updateBoardSchema = Joi.object({
